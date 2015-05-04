18bf4af0f40e901f190c813f2ed65853daa25772
Jonas hansen <jonas.hansen@bluewin.ch>
2015-05-04 11:19:28 +0200

    Added size method on GameBoard

--- src/ch/hslu/prg2/dotsandboxes/game/GameBoard.java
+++ src/ch/hslu/prg2/dotsandboxes/game/GameBoard.java
@@ -1,6 +1,11 @@
 package ch.hslu.prg2.dotsandboxes.game;
 
-
 public interface GameBoard {
 	PlayerColor getLineColor(Dot dot1, Dot dot2);
+
+	/**
+	 * 
+	 * @return the size of the game board. The board is always a square.
+	 */
+	int size();
 }
