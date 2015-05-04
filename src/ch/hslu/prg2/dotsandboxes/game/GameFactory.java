9022468ee3a2ce0df85161df36470aded2fae391
Jonas hansen <jonas.hansen@bluewin.ch>
2015-05-04 11:24:00 +0200

    Renamed method, added size as parameter

--- src/ch/hslu/prg2/dotsandboxes/game/GameFactory.java
+++ src/ch/hslu/prg2/dotsandboxes/game/GameFactory.java
@@ -1,12 +1,12 @@
 package ch.hslu.prg2.dotsandboxes.game;
 
 public class GameFactory {
-	public static Game createGUIvsNetworkGame() {
+	public static Game createGUIvsNetworkGame(int size) {
 		// TODO implement this method
 		return null;
 	}
 
-	public static Game createGUIvsKIGame() {
+	public static Game createGUIvsAIGame(int size) {
 		// TODO implement this method
 		return null;
 	}
