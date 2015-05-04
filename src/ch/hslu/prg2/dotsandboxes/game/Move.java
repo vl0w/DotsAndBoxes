e24137d158be7a5c9807101d938516ba28c8dbb1
Jonas hansen <jonas.hansen@bluewin.ch>
2015-05-04 11:49:42 +0200

    Added constructor

--- src/ch/hslu/prg2/dotsandboxes/game/Move.java
+++ src/ch/hslu/prg2/dotsandboxes/game/Move.java
@@ -1,8 +1,12 @@
 package ch.hslu.prg2.dotsandboxes.game;
 
-
 public class Move {
-	private Dot dot1;
-	private Dot dot2;
+	private Dot dotOne;
+	private Dot dotTwo;
 	private PlayerColor owner;
+
+	public Move(Dot dotOne, Dot dotTwo) {
+		this.dotOne = dotOne;
+		this.dotTwo = dotTwo;
+	}
 }
