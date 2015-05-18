296047a0d9333ce51040e1f92197e139ed454b5a
Jonas hansen <jonas.hansen@bluewin.ch>
2015-05-18 16:26:34 +0200

    Changed package name

--- src/chhslu/prg2/dotsandboxes/view/GameView.java
+++
@@ -1,9 +0,0 @@
-package ch.hslu.prg2.dotsandboxes.view;
-
-import ch.hslu.prg2.dotsandboxes.model.GameBoard;
-
-public interface GameView {
-	void update(GameBoard gameBoard);
-
-	void addViewListener(GameViewListener listener);
-}
--- src/chhslu/prg2/dotsandboxes/view/GameViewListener.java
+++
@@ -1,7 +0,0 @@
-package ch.hslu.prg2.dotsandboxes.view;
-
-import ch.hslu.prg2.dotsandboxes.model.Dot;
-
-public interface GameViewListener {
-	void onLineSelected(Dot dotOne, Dot dotTwo);
-}
