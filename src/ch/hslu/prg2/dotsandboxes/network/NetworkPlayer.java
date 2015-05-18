62b36ebbbf18eb13dc4fd0d10361dba4d34faf15
hksuh <hksuh@users.noreply.github.com>
2015-05-18 09:01:55 +0200

    Updated GameServer and NetworkPlayer

--- src/ch/hslu/prg2/dotsandboxes/network/NetworkPlayer.java
+++ src/ch/hslu/prg2/dotsandboxes/network/NetworkPlayer.java
@@ -4,7 +4,25 @@ import ch.hslu.prg2.dotsandboxes.Player;
 import ch.hslu.prg2.dotsandboxes.game.GameBoard;
 import ch.hslu.prg2.dotsandboxes.game.Move;
 
-public class NetworkPlayer implements Player {
+import java.io.IOException;
+import java.net.Socket;
+
+public class NetworkPlayer implements Player, Runnable {
+	private Socket clientSocket;
+
+	public NetworkPlayer(Socket client) {
+		clientSocket = client;
+	}
+
+	@Override
+	public void run() {
+		// Ab jetzt werden Spielzuege hin- und hergeschickt
+		try {
+
+		} catch (IOException ioe) {
+			System.out.println("Error: " + ioe.getMessage());
+		}
+	}
 
 	@Override
 	public Move makeMove(GameBoard board) {
