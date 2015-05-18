16a59c0fb060d39e1b43a0a8897076fe8610ec9d
Jonas hansen <jonas.hansen@bluewin.ch>
2015-05-18 17:27:46 +0200

    GameServer responds with true or false whether the incoming connection has been accepted

--- src/ch/hslu/prg2/dotsandboxes/network/GameServer.java
+++ src/ch/hslu/prg2/dotsandboxes/network/GameServer.java
@@ -1,6 +1,7 @@
 package ch.hslu.prg2.dotsandboxes.network;
 
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,35 +13,45 @@ import java.net.Socket;
 public class GameServer {
 	private static final int PORT_NUMBER = 7777;
 	private ServerSocket serverSocket;
-    private IncomingRequestHandler requestHandler;
+	private IncomingRequestHandler requestHandler;
 
-    public GameServer(IncomingRequestHandler requestHandler) {
-        this.requestHandler = requestHandler;
-    }
-
-	public void startListen() throws IOException {
-        serverSocket = new ServerSocket(PORT_NUMBER);
-        IncomingConnectionListener icl = new IncomingConnectionListener();
-        new Thread(icl).start();
+	public GameServer(IncomingRequestHandler requestHandler) {
+		this.requestHandler = requestHandler;
 	}
 
-    private class IncomingConnectionListener implements Runnable {
-        @Override
-        public void run() {
-            while (true) {
-                try {
-                    System.out.println("Warten auf Verbindung ...");
-                    Socket client = serverSocket.accept();
-                    System.out.println("Verbunden mit " + client.getInetAddress());
-                    if(requestHandler.acceptRequest()){
-                        new Thread(new IncomingObjectListener(client, new RemoteNetworkListenerDelegate())).start();
-                    }else{
-                        // fehler zurückmelden oder nichts tun
-                    }
-                } catch (IOException ioe) {
-                    System.err.println("Error: " + ioe.getMessage());
-                }
-            }
-        }
-    }
+	public void startListen() throws IOException {
+		serverSocket = new ServerSocket(PORT_NUMBER);
+		IncomingConnectionListener icl = new IncomingConnectionListener();
+		new Thread(icl).start();
+	}
+
+	private class IncomingConnectionListener implements Runnable {
+		@Override
+		public void run() {
+			while (true) {
+				try {
+					System.out.println("Warten auf Verbindung ...");
+					Socket client = serverSocket.accept();
+					System.out.println("Verbunden mit "
+							+ client.getInetAddress());
+					if (requestHandler.acceptRequest()) {
+						new Thread(new IncomingObjectListener(client,
+								new RemoteNetworkListenerDelegate())).start();
+						writeAnswer(client, true);
+					} else {
+						writeAnswer(client, false);
+					}
+				} catch (IOException ioe) {
+					System.err.println("Error: " + ioe.getMessage());
+				}
+			}
+		}
+
+		private void writeAnswer(Socket socket, boolean answer)
+				throws IOException {
+			ObjectOutputStream outStream = new ObjectOutputStream(
+					socket.getOutputStream());
+			outStream.writeBoolean(answer);
+		}
+	}
 }
