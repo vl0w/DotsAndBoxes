5410d6ed997c13308fea337041e619f9963aa6fe
Lukas <lukasschoepfer@sunrise.ch>
2015-05-19 11:37:33 +0200

    Javadoc Dokumentation

--- src/ch/hslu/prg2/dotsandboxes/model/GameBoard.java
+++ src/ch/hslu/prg2/dotsandboxes/model/GameBoard.java
@@ -1,5 +1,11 @@
 package ch.hslu.prg2.dotsandboxes.model;
 
+/**
+* Dient zum Verwalten der Linien, Squares und dessen aktuellen Zustand.
+* Über das Interface können die Grösse  des Spielfeldes und der aktuelle Punktestand abgefragt weren.
+*/
+
+
 public interface GameBoard {
 	PlayerColor getLineColor(Dot dot1, Dot dot2);	
 	/**
--- src/ch/hslu/prg2/dotsandboxes/model/GameModel.java
+++ src/ch/hslu/prg2/dotsandboxes/model/GameModel.java
@@ -1,5 +1,10 @@
 package ch.hslu.prg2.dotsandboxes.model;
 
+/**
+* Verarbeitet den Spielzug und informiert über erfolgreichem bzw. nicht erfolgreichem Zug.
+*/
+
+
 public interface GameModel {
 	void handleMove(Move move);
 
