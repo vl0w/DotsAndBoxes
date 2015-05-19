6dd4877b57b4f8169ab933a85cf7f56dec635bb7
Lukas <lukasschoepfer@sunrise.ch>
2015-05-19 11:44:35 +0200

    gamemodellistener javadoc gemacht

--- src/ch/hslu/prg2/dotsandboxes/model/GameModelListener.java
+++ src/ch/hslu/prg2/dotsandboxes/model/GameModelListener.java
@@ -1,5 +1,9 @@
 package ch.hslu.prg2.dotsandboxes.model;
 
+/**
+* Interessierte Klassen welche Events des Gamemodels empfangen wollen können sich als GameModelListener beim GameModel registrieren.
+*/
+
 public interface GameModelListener {
 	void moveDone(GameBoard gameBoard, MoveResult result);
 
