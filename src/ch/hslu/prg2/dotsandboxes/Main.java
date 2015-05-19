6addfc500a792a2b9156dce44fba175516071e30
Jonas hansen <jonas.hansen@bluewin.ch>
2015-05-19 09:25:27 +0200

    Try to show dialog on event dispatch queue

--- src/ch/hslu/prg2/dotsandboxes/Main.java
+++ src/ch/hslu/prg2/dotsandboxes/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.UnknownHostException;
 
 import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
 
 import ch.hslu.prg2.dotsandboxes.network.GameServer;
 import ch.hslu.prg2.dotsandboxes.network.IncomingRequestHandler;
@@ -22,20 +23,45 @@ public class Main {
 
 			@Override
 			public boolean acceptRequest() {
-				int result = JOptionPane.showConfirmDialog(null,
-						"New Request received from Player /n" + "with IP: "
-								+ "unknown" + "./n"
-								+ "Would you like to start the game?",
-						"Request Received", JOptionPane.YES_NO_OPTION);
-				if (result == 0) {
-					return true;
-				} else {
-					return false;
+				MsgDiag dialog = new MsgDiag();
+				try {
+					SwingUtilities.invokeAndWait(dialog);
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
+				return dialog.isRequestAccepted();
 			}
+
 		};
 
 		GameServer server = new GameServer(requestHandler);
 		server.startListen();
+	}
+
+	static class MsgDiag implements Runnable {
+
+		private boolean requestAccepted;
+
+		public MsgDiag() {
+			requestAccepted = false;
+		}
+
+		public boolean isRequestAccepted() {
+			return requestAccepted;
+		}
+
+		@Override
+		public void run() {
+			int result = JOptionPane.showConfirmDialog(null,
+					"New Request received from Player /n" + "with IP: "
+							+ "unknown" + "./n"
+							+ "Would you like to start the game?",
+					"Request Received", JOptionPane.YES_NO_OPTION);
+			if (result == 0) {
+				requestAccepted = true;
+			} else {
+				requestAccepted = false;
+			}
+		}
 	}
 }
